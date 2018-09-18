413029aa669840f1cd2a98fd6882aeadd7da13dc
Sena Sahin <sena@bamboo.gtisc.gatech.edu>
2018-09-18 15:56:13 +0000

    updated attack and removed model

--- attack.py
+++ attack.py
@@ -22,31 +22,34 @@ def parse_tree(tree_):
     right = tree_.children_right
 
     threshold = tree_.threshold
+    #TODO- Our sapmle number and total of value number doesn't mach
     value = tree_.value
     node = 0 ## root is located at index 0 in the array
     features = tree_.feature
 
-    recursive_parse(left, right, threshold, features, value, node)
+    recursive_parse(left, right, threshold, features, node, value)
 
-    quit()
+    
 
-
+    #https://www.kdnuggets.com/2017/05/simplifying-decision-tree-interpretation-decision-rules-python.html
 def recursive_parse(left, right, threshold, features, node, value, depth=0):
     indent = "\t" * depth
     if(threshold[node] != -2):
-        print indent, "if ( " + str(features[node]) + " <= " + str(threshold[node]) + " || " + value[node]  + "  ) { " 
+        print indent, "if ( " + str(features[node]) + " <= " + str(threshold[node])  + " class:" + str(np.argmax(value[node])) +  "  ) { " 
         if left[node] != -1:
-            print 'left'
+            recursive_parse (left, right, threshold, features, left[node], value, depth+1)
             print indent, '} else { '
             if right[node] != -1:
-                print 'right'
+                recursive_parse (left, right, threshold, features, right[node], value, depth+1)
             print indent, ' } '
         
+    else:
+        print indent,"return "  + str(value[node])
+                                                                                                                                                        
 
 
 
-
-def find_tree(clf):
+def find_tree(clf,api_list):
     #https://stats.stackexchange.com/questions/118016/how-can-you-print-the-decision-tree-of-a-randomforestclassifier
     i_tree = 0
 
@@ -68,13 +71,13 @@ def find_tree(clf):
         # Write tree information to dot file
         with open(fn, 'w') as my_file:
             #fn.get._state_()
-            my_file = tree.export_graphviz(tree_in_clf, out_file = my_file)
+            my_file = tree.export_graphviz(tree_in_clf, out_file = my_file, class_names = ['benign', 'malicious'], feature_names = api_list)
             #my_file.get._state_()
         # Convert dot file to png file
         #call(['dot', '-Tpng', fn, '-o', outfn, '-Gdpi=600'])
 
         i_tree = i_tree + 1
-
+        quit()
 
 
 def usage():
@@ -82,15 +85,27 @@ def usage():
     sys.exit(2)
 
 def _main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         usage()
 
     sample_dir = sys.argv[1]
+    api_file = sys.argv[2]
+
+    # Get number of API calls
+    api_list = []
+    a = 0
+    with open(api_file, 'rb') as fr:
+        for line in fr:
+            api_list.append(str(a))
+            a = a+1
+    #print api_list
+    #quit()
+
 
     clf = joblib.load('model.pkl')
     print clf
 
-    find_tree(clf)
+    find_tree(clf,api_list)
     for root, dirs, files in os.walk(sample_dir):  
         for filename in files:
             # Ignore metadata
